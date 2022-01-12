d34b668eb57fc7b95e0797ae633da0f6793274cc
Ray Vílchez <56769871+rayvilchez@users.noreply.github.com>
2022-01-12 18:53:53 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            double poundValue = 205.62d;
+            double kgValue = (poundValue * 0.45359237);
+
+            System.out.println("If I weight " + poundValue + " pounds, how many Kg do I weight??");
+            System.out.println("You weight is " + kgValue + "Kg");
+    }
+}
